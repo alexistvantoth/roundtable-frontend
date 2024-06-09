d6bba7b2e2d5d8fb859dcac81143dc32dd5cb191
alexistvantoth <alex.istvan.toth@gmail.com>
2024-06-09 11:28:54 +0200

    basic layout for the chat app with grids

--- src/chat/chat.component.ts
+++ src/chat/chat.component.ts
@@ -5,19 +5,30 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {
   FormControl,
+  FormsModule,
   ReactiveFormsModule,
-  UntypedFormControl,
   UntypedFormGroup,
 } from '@angular/forms';
 import { Message } from '../app/models/message.model';
 import { CookieService } from 'ngx-cookie-service';
 import { MatInputModule } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, MatInputModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   providers: [],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss',
